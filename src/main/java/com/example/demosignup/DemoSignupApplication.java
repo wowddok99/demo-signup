3d9631a07e86434058ce8ad979e158310161cedb
wowddok99 <wowddok99@gmail.com>
2023-05-22 01:34:35 +0900

    initial settings

---
+++ src/main/java/com/example/demosignup/DemoSignupApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSignupApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1_0_1__add_tb_member.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS member (
+    id              BIGINT              AUTO_INCREMENT              PRIMARY KEY,
+    username        VARCHAR(255)                                                        COMMENT '계정',
+    password        VARCHAR(255)                                                        COMMENT '비밀번호',
+    nickname        VARCHAR(255)                                                        COMMENT '본명(Full-name)',
+    age             INT                                                                 COMMENT '연령',
+    status          VARCHAR(30)         DEFAULT 'PENDING',
+    join_date       DATETIME(6)         NOT NULL                    DEFAULT CURRENT_TIMESTAMP(6),
+
+    CONSTRAINT      uq_member_username  UNIQUE (username)
+);
